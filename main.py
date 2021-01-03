1991fdc2ef557ff52b6cb2be2aefe071b31e4f8c
Gsvd <me@gsvd.dev>
2021-01-03 16:45:00 +0100

    handle ipv6

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
+import os
 from requests import get
 from socket import gethostbyname
 from json import loads
 from datetime import datetime
 
 
-def get_public_ip():
+def get_public_ipv4():
+    tmp = get(r'http://ipv4.jsonip.com')
+    return tmp.json()['ip']
+
+def get_public_ipv6():
     tmp = get(r'http://jsonip.com')
     return tmp.json()['ip']
 
-def update_ip(username, password, dns, ip, provider):
+def update_ip(username, password, dns, ip, provider, dt_string):
     url = provider.format(dns=dns, ip=ip)
     tmp = get(url, auth=(username, password))
+    if tmp.status_code == 200:
+        print("[{date}] Successful: {dns} set to {ip}".format(date=dt_string, dns=dns, ip=ip))
+    else:
+        print("[{date}] Error: {dns} set to {ip}".format(date=dt_string, dns=dns, ip=ip))
     return True if tmp.status_code == 200 else False
-
-def check_dns_ip(dns, dns_ip):
-    try:
-        return gethostbyname(dns) == dns_ip
-    except:
-        return False
 
 if __name__ == "__main__":
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    public_ip = get_public_ip()
-    with open("config.json", "r") as content_file:
+    public_ipv4 = get_public_ipv4()
+    public_ipv6 = get_public_ipv6()
+    with open("{}/config.json".format(os.path.dirname(__file__)), "r") as content_file:
         content = content_file.read()
         entries = loads(content)["entries"]
         for entry in entries:
-            if not check_dns_ip(entry["dns"], public_ip):
-                if update_ip(entry["username"], entry["password"], entry["dns"], public_ip, entry["provider"]):
-                    print("[{date}] Successful: {dns} set to {ip}".format(date=dt_string, dns=entry["dns"], ip=public_ip))
-                else:
-                    print("[{date}] Error: {dns} set to {ip}".format(date=dt_string, dns=entry["dns"], ip=public_ip))
+            update_ip(entry["username"], entry["password"], entry["dns"], public_ipv4, entry["provider"], dt_string)
+            if public_ipv4 != public_ipv6:
+                update_ip(entry["username"], entry["password"], entry["dns"], public_ipv6, entry["provider"], dt_string)
